23fe63d1ffd4ca34855b218b78ce7ab7b0a723e5
luolawlan <luolawlan@outlook.com>
2023-12-31 10:52:02 +0800

    launchprogram.lua have some error

---
+++ launchprogram.lua
@@ -0,0 +1,9 @@
+local launchprogram = {}
+
+--- default program
+launchprogram.default_prog = { '/usr/bin/zsh', '-1'}
+
+--- working directory
+launchprogram.default_cwd = "$HOME/Code"
+
+return launchprogram
--- wezterm.lua
+++ wezterm.lua
@@ -1,10 +1,17 @@
 --- require lua module
 local wezterm = require 'wezterm'
 local appearance = require 'appearance'
+local launchprogram = require 'launchprogram'
 
 local config = {}
 
 --- color_scheme "Apple Classic"
 config.color_scheme = appearance.color_scheme
+
+--- default program
+config.default_prog = launchprogram.default_prog
+
+--- default working directory
+config.default_cwd = launchprogram.default_cwd
 
 return config
