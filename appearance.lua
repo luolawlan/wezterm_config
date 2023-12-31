adecf4d664097b912931b5c49d9d0d27e2bd4059
luolawlan <luolawlan@outlook.com>
2023-12-31 10:33:06 +0800

    appearance

---
+++ appearance.lua
@@ -0,0 +1,5 @@
+local appearance = {}
+
+appearance.color_scheme = 'Apple Classic'
+
+return appearance 
--- wezterm.lua
+++ wezterm.lua
@@ -1,6 +1,10 @@
---- require wezterm
+--- require lua module
 local wezterm = require 'wezterm'
+local appearance = require 'appearance'
+
 local config = {}
 
+--- color_scheme "Apple Classic"
+config.color_scheme = appearance.color_scheme
 
 return config
